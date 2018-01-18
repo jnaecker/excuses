6ac5f493954ebc7d4ab50975046ed6fe2916492e
coonstiffany <tcoons@wesleyan.edu>
2018-01-18 16:34:01 -0500

    finished commenting donations app

--- oTree/donation/views.py
+++ oTree/donation/views.py
@@ -3,16 +3,19 @@ from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-
+#This class controls the Donation lists of the app
 class Donation(Page):
-
+    #Creates the interaction rows that user selects for each row
     form_model = models.Player
     form_fields = ['donation_{}'.format(i) for i in range(0, Constants.num_rows)]
     
+    #This function is run after the user makes their selections for each row and 
+    #hits enters but before the round results is run.
     def before_next_page(self):
-
+        #In this try catch statements we obtain where the users switches from deciding
+        #to give themselves the money to giving it to charity, i.e when they switch from 
+        #true to false. If they never switch it defaults to the max number of rows
         try:
-            # find normalized payoff
             self.session.vars['donation_amount'] = [self.player.donation_0,
                                                     self.player.donation_1,
                                                     self.player.donation_2,
@@ -26,9 +29,11 @@ class Donation(Page):
         except ValueError:
             self.session.vars['donation_amount'] = Constants.num_rows
 
-
+        #Calls set_func function in models.py that creates array of donation amounts 
+        #that can be passed on
         self.player.set_func()
 
+        #Calls the set_payoff function in models.py  
         self.player.set_payoffs()
 
 
@@ -45,7 +50,7 @@ class Donation(Page):
             'char_prob' : opp_prob
     	}
 
-
+#This class controls the final results of all rounds of the donation app
 class Results(Page):
     
     def is_displayed(self):
@@ -60,6 +65,7 @@ class Results(Page):
             'ro_don' : self.participant.vars['round_donations']
         }      
 
+#This class controls the results displayed after each round of the donation app
 class RoundResults(Page):
     
     def vars_for_template(self):
